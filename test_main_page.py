702b895f6d9f8eb38af238092cbb9f45c58f4453
dark-tulip <t.telbayeva@mail.ru>
2021-09-21 17:22:47 +0600

    added page object to test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,12 @@
+from .pages.main_page import MainPage
+
+
 def test_guest_can_go_to_login_page(browser):
 
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    # Создаем экземпляр страницы - передаем экземпляр драйвера и адрес страницы
+    page = MainPage(browser, link)
+
+    # Открываем страницу и переходим по ссылке
+    page.open()
+    page.go_to_login_page()

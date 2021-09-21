8b1473b8d2f02ee24f42378c491bf7a52dc6f6d2
dark-tulip <t.telbayeva@mail.ru>
2021-09-21 19:46:32 +0600

    Modified Base Page class

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,14 +1,15 @@
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
 
 
 class MainPage(BasePage):
 
     def go_to_login_page(self):
-
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        # Звездочка для автораспаковки кортежа
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
 
     def should_be_login_link(self):
+        # Найдена ссылка для регистрации
         assert self.is_element_present(
-            By.CSS_SELECTOR, "login_link_invalid"), "******Login link NOT FOUND******"
+            *MainPageLocators.LOGIN_LINK), "**** Login link NOT FOUND ****"
--- test_main_page.py
+++ test_main_page.py
@@ -2,13 +2,15 @@ from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-
+    '''
+        Тест кейс для проверки того что новый юзер может зарегистрироваться
+    '''
     link = "http://selenium1py.pythonanywhere.com/"
+
     # Создаем экземпляр страницы - передаем экземпляр драйвера и адрес страницы
     page = MainPage(browser, link)
 
     # Открываем страницу и переходим по ссылке
     page.open()
     page.go_to_login_page()
-
     page.should_be_login_link()
